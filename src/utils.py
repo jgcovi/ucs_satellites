ed72bc60f40c1486f85d3fa23060caec24707991
jessie <jgcovi@gmail.com>
2021-09-26 21:46:11 -0400

    generalize some web scraper functions

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,12 @@ def get_files_in_dir(dirname='data'):
     if not os.path.exists(project_dir):
         os.mkdir(new_dir)
     return os.listdir(new_dir)
+
+def split_url(url):
+    """"Split a given URL into the protocol, domain, and path for the use of building linked URLs."""
+    # Get info to build urls later
+    url_split = url.split('//')
+    protocol = url_split[0]  # get transfer protocol
+    domain = url_split[1].split('/')[0]  # get url domain
+
+    return protocol, domain
--- src/web_scraper.py
+++ src/web_scraper.py
@@ -1,45 +1,53 @@
 from datetime import datetime
+from functools import update_wrapper
 import requests
 from bs4 import BeautifulSoup
 
-from utils import get_files_in_dir
+import utils
 
-def get_date_updated_on(soup):
-    """Get the date of the last page update."""
-    updated_on_str = soup.find_all('span', attrs={'class': "date-updated"})[0].contents[0]
-    updated_on_str = updated_on_str.lower().lstrip('updated ')
-    return datetime.strptime(updated_on_str, '%B %d, %Y')
-
-def save_file(file_url, fname):
+#### General webscraping functions
+def save_web_file(file_url, fname):
     """Download the file from the url."""
     req = requests.get(file_url, allow_redirects=True)
     print(fname)
     with open(fname, 'wb') as output:
         output.write(req.content)
 
+def get_webpage_contents(url):
+    """Return webpage contents from a url."""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    updated_on = get_ucs_date_updated_on(soup)
+
+    return soup, updated_on
+
+#### Page specific functions
+
+# Union of Concerned Scientists webpage
+def get_ucs_date_updated_on(soup):
+    """Get the date of the last page update."""
+    updated_on_str = soup.find_all('span', attrs={'class': "date-updated"})[0].contents[0]
+    updated_on_str = updated_on_str.lower().lstrip('updated ')
+    return datetime.strptime(updated_on_str, '%B %d, %Y')
         
 def get_file_from_ucs_page(url='https://www.ucsusa.org/resources/satellite-database', data_dir='data'):
     """Read the UCS webpage to determine if there is a new file available. If yes, then save it."""
-    # Get info to build urls later
-    protocol = url.split('//')[0]  # get transfer protocol
-    domain = url.split('//')[1].split('/')[0]  # get url domain
-    # Get the url contents
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    updated_on = get_date_updated_on(soup)
+    protocol, domain = utils.split_url(url)  # get the transfer protocol and domain to build linked url later
+    soup, updated_on = get_webpage_contents(url)
 
     # Create filename with updated_on date 
-    read_fname = data_dir + '/usc_satellites_' + datetime.strftime(updated_on, '%Y%b%d') + '.xls'
+    sat_fname = 'usc_satellites_' + datetime.strftime(updated_on, '%Y%b%d') + '.xls'
+    save_sat_fname = data_dir + '/' + sat_fname
+    
     # Get the contents of the data_dir file where the file should be located, if already saved
-    data_files = get_files_in_dir(data_dir)
+    my_files = utils.get_files_in_dir(data_dir)
     # if the data dir is empty OR a filename matching the most recent version is not in the dir, save the new file
-    if (len(data_files) == 0) | (not any([f == read_fname for f in data_files])):  
+    if (len(my_files) == 0) | (not sat_fname in my_files):  
         db_filelist = []
         for a in soup.find_all('a', href=True):
             if ('/media/' in a['href']) & (a.contents[0] == 'Database'):
                 db_filelist.append(a['href'])
-        file_path = db_filelist[0]  # doesn't matter which one we save as long as we save to excel
+        db_url_path = db_filelist[0]  # doesn't matter which one we save as long as we save to excel
         # both links work saving to excel, but one format does not save to csv well
-        save_file(protocol + '//' + domain + file_path, read_fname)
-    
-    return read_fname
+        save_web_file(protocol + '//' + domain + db_url_path,  save_sat_fname)
+    return save_sat_fname
